e2d971456585102517c63d209df1ab9a6fe222b2
Jakub Šindelář <megastary@gmail.com>
2020-08-27 11:09:20 +0200

    Added Dockerfile, axios Dashboard

--- App.Dashboard/index.js
+++ App.Dashboard/index.js
@@ -4,52 +4,28 @@ const app = express()
 const port = 3000
 const axios = require('axios')
 
-// Variables
-const computersApp = 'http://localhost:3002/'
-const usersApp = 'http://localhost:3001/'
+// Module Computers and Users are currently working on same machine
+// TODO: Do it correctly for kubernetes cluster
+const computersApp = `http://${process.env.COMPUTERNAME}.${process.env.USERDNSDOMAIN}:3002/`
+const usersApp = `http://${process.env.COMPUTERNAME}.${process.env.USERDNSDOMAIN}:3001/`
 
-// app.get('/', (req, res) => {
-//   let response = '<h1>DashBoard</h1>'
-//   axios.get(computersApp).then(result => {
-//     console.log('Computer data fetched successfully.')
-//   }).catch(error => {
-//     console.log('Computer data fetch failed with error: ' + error)
-//   }).then(computer => {
-//     response += `<p>${computer}</p>`
-//     axios.get(usersApp).then(result => {
-//       console.log('User data fetched successfully.')
-//     }).catch(error => {
-//       console.log('Computer data fetch failed with error: ' + error)
-//     }).then(user => {
-//       response += `<p>${user}</p>`
-//       res.send(response)
-//     })
-//     })
-// })
+// Create axios httpClient instance and set timeout to prevent long requests when other components are not responding
+const httpClient = axios.create();
+httpClient.defaults.timeout = 1000;
 
-// app.get('/', (req, res) => {
-//   axios.all([
-//     axios.get(computersApp),
-//     axios.get(usersApp)
-//   ]).then(responseArray => {
-//     console.log(responseArray[0].data)
-//     console.log(responseArray[1].data)
-//   })
-// })
-
+// Root path will fetch data from Computers and Users and respond with simple HTML page
 app.get('/', (req, res) => {
-  axios({
-    method: 'get',
-    url: `http://${process.env.COMPUTERNAME}:3000/test`
+  let response = '<h1>DashBoard</h1>'
+  axios.all([
+    httpClient.get(computersApp),
+    httpClient.get(usersApp)
+  ]).then(responseArray => {
+    response += `<p>${responseArray[0].data}</p><p>${responseArray[1].data}</p>`
+    res.send(response)
+  }).catch(error => {
+    response += `<p>Failed to fetch data</p><p>${error}</p>`
+    res.send(response)
   })
-    .then(function (response) {
-      console.log(response)
-      res.send('Hello from root')
-    });
-})
-
-app.get('/test', (req, res) => {
-  res.send('Hello')
 })
 
 app.listen(port, () => {
